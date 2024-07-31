6abf38bfcc541cedd2185e43f2ad68f7c1574e4e
celest <1269504317@qq.com>
2024-07-31 22:28:25 +0800

    实现player模型的左右移动及跳跃,以及边界的判断

--- static/js/player/player.js
+++ static/js/player/player.js
@@ -18,12 +18,12 @@ export class Player extends GameObject {
 
         this.gravity = 50;
 
-        this.seepdx = 400;//水平速度
-        this.seepdy = 1000;//垂直速度
+        this.seepdx = 600;//水平速度
+        this.seepdy = 2000;//垂直速度
 
         this.ctx = this.root.gameMap.ctx;
 
-        this.pressed_key = this.root.gameMap.controller.pressed_key;
+        this.pressed_keys = this.root.gameMap.controller.pressed_keys;
 
         this.status = 3 //0: idle 1:向前 2: 向后  3: 跳跃 4: 攻击  5: 被打 6: 死亡 
     }
@@ -31,15 +31,56 @@ export class Player extends GameObject {
 
     }
 
-
-
-
     update() {
-        this.move();
-        this.render();
+        this.update_controller();
+        this.update_move();
+        this.update_render();
     }
 
-    move() {
+    update_controller() {
+        let w, d, a, space;
+        // console.log(this.pressed_keys);
+        if (this.id === 0) {
+            w = this.pressed_keys.has('w');
+            d = this.pressed_keys.has('d');
+            a = this.pressed_keys.has('a');
+            space = this.pressed_keys.has(' ');
+
+        } else {
+            w = this.pressed_keys.has('ArrowUp');
+            d = this.pressed_keys.has('ArrowRight');
+            a = this.pressed_keys.has('ArrowLeft');
+            space = this.pressed_keys.has('Enter');
+        }
+
+
+        if (this.status === 1 || this.status === 0) {
+            if (w) {
+                if (d) {
+                    this.vx = this.seepdx;
+                } else if (a) {
+                    this.vx = -this.seepdx;
+                } else {
+                    this.vx = 0;
+                }
+                this.vy = -this.seepdy;
+                this.status = 3;
+            } else if (d) {
+                this.vx = this.seepdx;
+                this.status = 1;
+            } else if (a) {
+                this.vx = -this.seepdx;
+                this.status = 1;
+            } else {
+                this.vx = 0;
+                this.status = 0;
+            }
+            // console.log(this.vx, this.vy);
+        }
+
+    }
+
+    update_move() {
         this.vy += this.gravity;
 
         this.x += this.vx * this.timedeldt / 1000;
@@ -49,10 +90,20 @@ export class Player extends GameObject {
         if (this.y > 450) {
             this.y = 450;
             this.vy = 0;
+            if (this.status === 3) {
+                this.status = 0;
+            }
+        }
+
+        if (this.x < 0) {
+            this.x = 0;
+        }
+        if (this.x + this.width > this.root.gameMap.$canvas.width()) {
+            this.x = this.root.gameMap.$canvas.width() - this.width;
         }
 
     }
-    render() {
+    update_render() {
 
         this.ctx.fillStyle = this.color;//渲染玩家模型
 
