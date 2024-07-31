94c09f5a2dbedb4f5188f6929b33bf488c0a45bc
celest <1269504317@qq.com>
2024-07-31 20:32:50 +0800

    实现人物模型垂直下落,并在下降到一定高度静止

--- static/js/player/player.js
+++ static/js/player/player.js
@@ -14,6 +14,9 @@ export class Player extends GameObject {
         this.vx = 0;
         this.vy = 0;
 
+        this.difrection = 1;
+
+        this.gravity = 50;
 
         this.seepdx = 400;//水平速度
         this.seepdy = 1000;//垂直速度
@@ -24,14 +27,31 @@ export class Player extends GameObject {
 
     }
 
-    update() {
 
+
+
+    update() {
+        this.move();
         this.render();
     }
 
+    move() {
+        this.vy += this.gravity;
+
+        this.x += this.vx * this.timedeldt / 1000;
+        this.y += this.vy * this.timedeldt / 1000;
+        // console.log(this.vy * this.timedeldt / 1000);
+        // console.log(this.vy)
+        if (this.y > 450) {
+            this.y = 450;
+            this.vy = 0;
+        }
+
+    }
     render() {
 
         this.ctx.fillStyle = this.color;//渲染玩家模型
+
         this.ctx.fillRect(this.x, this.y, this.width, this.height);
         // console.log(this.color);
     }
