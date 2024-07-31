8be4b1e219065dc2da87db30770bba62d1023ba8
celest <1269504317@qq.com>
2024-07-31 18:44:48 +0800

    使用canvas创建游戏地图,供渲染使用

--- static/js/base.js
+++ static/js/base.js
@@ -1,7 +1,10 @@
+import { GameMap } from "./game_map/base.js";
+
 class KOF {
     constructor(id) {
         this.$kof = $('#' + id);
-        console.log(this.$kof);
+        // console.log(this.$kof);
+        this.gameMap = new GameMap(this);
     }
 }
 export {
